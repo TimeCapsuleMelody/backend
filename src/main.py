fc81ae3dd138e66f8230f41a69d184bbacaa081a
minjaekweon <mjkweon17@korea.ac.kr>
2025-01-18 21:34:07 +0900

    feat: friend mock api

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-from typing import Union, List
+from typing import Union
 from pydantic import BaseModel
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from route.friend_route import router as friend_route
+
 
 app = FastAPI(
     title="Time Capsule Melody",
@@ -32,6 +36,7 @@ app.add_middleware(
     expose_headers=["Access-Control-Allow-Origin", "Authorization"]
 )
 
+app.include_router(friend_route)
 
 @app.get("/")
 def read_root():
@@ -43,38 +48,3 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-class FriendsWithStatisticResponse(BaseModel):
-    id: int
-    name: str
-    image: str
-    totalCount: int
-    localCount: int
-    ratio: float
-
-
-@app.get("/friends/with-statistic", response_model=List[FriendsWithStatisticResponse])
-def get_friends_with_statistic():
-    # 예시 데이터
-    friends_data = [
-        {
-            "id": 1,
-            "name": "루피",
-            "image": "https://image-cdn.hypb.st/https%3A%2F%2Fkr.hypebeast.com%2Ffiles%2F2022%2F08%2Fone-piece-luffys-anime-voice-actress-says-she-doesnt-read-the-manga-01.jpg",
-            "totalCount": 100,
-            "localCount": 80,
-            "ratio": 0.8
-        },
-        {
-            "id": 2,
-            "name": "조로",
-            "image": "https://mblogthumb-phinf.pstatic.net/20091005_167/tsuki924_1254712963371ULps1_jpg/%C1%B6%B7%CE_tsuki924.jpg",
-            "totalCount": 50,
-            "localCount": 45,
-            "ratio": 0.9
-        }
-    ]
-
-    # ratio를 기준으로 내림차순 정렬
-    sorted_friends = sorted(
-        friends_data, key=lambda x: x["ratio"], reverse=True)
-    return sorted_friends
